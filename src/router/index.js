231eddc7c149e77b4642342d27068fe2229abf93
Reno Ardian Syaputra <reno.ardian@aqi.co.id>
2024-08-29 08:25:03 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Auth/LoginView.vue'
+import Home from '../views/Home/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+  ]
+})
+
+export default router
---
+++ src/views/Auth/LoginView.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+  <button class="btn w-64 rounded-full btn-neutral">Button</button>
+</template>
